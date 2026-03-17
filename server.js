9cb5a14858fac0827f7918a172fec6ec4bf87b77
S-aiquipa <saiquipapoma@gmail.com>
2026-03-17 15:55:50 -0500

    Fix code

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.json());
 // 1. Connect to MongoDB
 mongoose
   .connect(process.env.MONGO_URI)
-  .then(async () => {
+  .then(() => {
     console.log("✅ Connected to MongoDB");
   })
   .catch((err) => console.error("❌ MongoDB connection error:", err));
@@ -271,6 +271,7 @@ app.post("/login", async (req, res) => {
 });
 
 // 4. Start server
-app.listen(5000, () => {
-  console.log("🚀 Server running on http://localhost:5000");
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
